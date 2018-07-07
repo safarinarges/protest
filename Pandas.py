8d9ed8e79ba7fc8b5f3d1f37ca82efe00e13cef0
Narges <nsafari@aut.ac.nz>
2018-07-07 14:56:55 +1200

    Initial pandas code

---
+++ Pandas.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 14:13:13 2017
+
+@author: apple
+"""
+
+import pandas as pd
+import matplotlib as plt
+from matplotlib import style
+style.use('ggplot')
+
+
+
+
+
+web_data={"posts":[1,2,3,4,5,6],
+          "shares":[3,2,5,7,3,9],
+          "likes":[6,7,6,4,8,8]}
+wd=pd.DataFrame(web_data)
+print(wd)
