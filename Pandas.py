b101f10f67c7d0c4545d786cda0150e81cdd0631
Narges <nsafari@aut.ac.nz>
2018-07-07 15:04:43 +1200

    creating new pandas series

--- Pandas.py
+++ Pandas.py
@@ -12,7 +12,7 @@ from matplotlib import style
 style.use('ggplot')
 
 
-
+#%%
 
 
 web_data={"posts":[1,2,3,4,5,6],
@@ -20,3 +20,8 @@ web_data={"posts":[1,2,3,4,5,6],
           "likes":[6,7,6,4,8,8]}
 wd=pd.DataFrame(web_data)
 print(wd)
+
+#%%
+# Creating new cols
+col_1 = pd.Series([1,2,3,4])
+col_2 = pd.Series(['a','b','c','d'])
